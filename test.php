e541b5721e1aa8c78a091557e172c6e4779d0096
Erik van Velzen <erik@evanv.nl>
2016-08-10 01:20:48 +0200

    Update test.php

--- test.php
+++ test.php
@@ -4,4 +4,4 @@ require 'src/Namespaced/Class1.php';
 
 require 'src/Class1.php';
 
-require 'src/Namespaced/Class2.php';
+require 'src/Namespaced/Class2.php'; // error: PHP Fatal error:  Cannot use Class1 as Class1 because the name is already in use in ./src/Namespaced/Class2.php on line 5
